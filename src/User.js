7d4ba0ad4ca5eeb2959a01f70e4304c9113bce56
alena-vovkohon <alena.vovkogon@gmail.com>
2022-05-22 15:43:21 +0300

    test

--- src/User.js
+++ src/User.js
@@ -10,7 +10,7 @@ import { Book } from './Book.js';
  * @property {User[]} friends
  * @property {Book[]} likes
  */
-export function User( name, date ) {
+export function User({ name, date } ) {
     this.name = name;
     this.date = date;
     this.myBooks = [];
@@ -62,8 +62,6 @@ export function User( name, date ) {
             })
         }
     };
-
-  
     // Remove Friends
     this.removeFriend = this.addToFriends;
 
